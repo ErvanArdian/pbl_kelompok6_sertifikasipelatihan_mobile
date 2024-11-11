d605ce42f17c74c45a1d6d2a3ab171cefb9a4b47
Dimas Rosyidin <dimasrosyidinn@gmail.com>
2024-11-11 09:48:28 +0700

    course

--- lib/screens/my_courses_page.dart
+++ lib/screens/my_courses_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 class MyCoursesPage extends StatelessWidget {
+  const MyCoursesPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('My Courses')),
-      body: Center(child: Text('List of your enrolled courses')),
+      appBar: AppBar(title: const Text('My Courses')),
+      body: const Center(child: Text('List of your enrolled courses')),
     );
   }
 }
